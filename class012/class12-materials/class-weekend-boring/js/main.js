c2d73000a58729ed324fa5e18b9abbcc17443344
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2024-02-13 19:10:24 +0100

    fixed a typo

--- class012/class12-materials/class-weekend-boring/js/main.js
+++ class012/class12-materials/class-weekend-boring/js/main.js
@@ -1,10 +1,14 @@
-document.querySelector('#check').addEventListener('click', check)
+document.querySelector("#check").addEventListener("click", check);
 
 function check() {
-
-  const day = document.querySelector('#day').value
+  const day = document.querySelector("#day").value;
 
   //Conditionals go here
-
-
+  if (day === "monday" || day === "tursday") {
+    console.log("class day");
+  } else if (day === "sonday" || day === "satrday") {
+    console.log("weekend");
+  } else {
+    console.log("boring  day");
+  }
 }
