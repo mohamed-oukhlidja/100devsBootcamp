5fa489cd0804f886c8ce212ca5c433bbf12019bc
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2024-02-14 18:05:01 +0100

    update the files

---
+++ class012/class12-materials/Homework/variables.js
@@ -0,0 +1,18 @@
+// Declare two variables: admin and name.
+let admin, name;
+
+// Assign the value "John" to name.
+name = "john";
+
+// Copy the value from name to admin.
+admin = name;
+
+// Show the value of admin using alert (must output “John”).
+console.log(admin);
+console.log(name);
+
+// Create a variable with the name of our planet. How would you name such a variable?
+let ourPlanet;
+
+//Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let currentUser;
