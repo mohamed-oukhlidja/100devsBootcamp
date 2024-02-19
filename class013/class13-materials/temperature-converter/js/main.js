6f30319bd3bbff4b0b4c232ea4fb8384e27f4999
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2024-02-19 21:40:29 +0100

    added a converter

--- class013/class13-materials/temperature-converter/js/main.js
+++ class013/class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,8 @@
-//Write your pseduo code first! 
+//Write your pseudo code first!
+document.querySelector('#check').addEventListener('click', convert);
+
+function convert() {
+  let valueInC = document.querySelector('#sel').value;
+  valueInC = (valueInC * 9) / 5 + 32;
+  document.querySelector("#placeToSee").innerText = valueInC;
+}
