a87bd2d16c8f7cd65fc79bd706e48d1cc6088225
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2024-11-11 18:07:13 +0100

    update

--- class014/class14-materials/bachelor-code/1/js/main.js
+++ class014/class14-materials/bachelor-code/1/js/main.js
@@ -2,5 +2,5 @@ document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
 
 function hide(){
 	document.querySelector(/*INSERTCODE*/).style.display = 'none'
-	document.querySelector(/*INSERTCODE*/).style.display = 'block'
+	document.querySelector(/*INSERTCODE*/).style.display = 'none'
 }
