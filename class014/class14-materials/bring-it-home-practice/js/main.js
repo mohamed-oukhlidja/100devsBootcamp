1a2eab1b2994400aa700f4bce100c88e62a98a67
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2024-11-13 22:15:09 +0100

    update the bring it home file

--- class014/class14-materials/bring-it-home-practice/js/main.js
+++ class014/class14-materials/bring-it-home-practice/js/main.js
@@ -1,15 +1,37 @@
 // *Variables*
 // Create a variable and console log the value
+let myAge = 26;
+console.log(myAge);
+
 
 // Create a variable, add 10 to it, and alert the value
 
+let myBirthYear = 1998;
+myBirthYear += 10;
+alert(myBirthYear)
+
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
+function subtracFourNumbers(num1, num2, num3, num4) {
+  alert(num1 - num2 - num3 - num4);
+}
 
 // Create a function that divides one number by another and returns the remainder
-
+function divides(num1, num2) {
+  return num1 % num2;
+}
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
-
+function isNumberGreaterThanFifty(num1, num2) {
+  if (num1 + num2 > 50) {
+    alert("jumanji");
+  }
+  alert("not jumanji");
+}
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
- 
+function isNumberDivisible(num1, num2, num3) {
+  if ((num1 * num2 * num3) % 3 === 0) {
+    alert("ZEBRA");
+  }
+  alert("not ZEBRA");
+}
--- class014/class14-materials/simple-functions/js/main.js
+++ class014/class14-materials/simple-functions/js/main.js
@@ -7,17 +7,17 @@ add(9, 10);
 add(4, 60);
 
 //create a function that multiplys three numbers and console logs the productfunction add(number1, number2) {
-function multiplys(number1, number2) {
-  alert(number1 * number2);
+function multiplys(number1, number2, number3) {
+  alert(number1 * number2 * number3);
 }
 
-multiplys(9, 10);
-amultiplysd(4, 60);
+multiplys(9, 10, 4);
+amultiplysd(4, 60, 8);
 
 //create a function that divides two numbers and returns the ???
 function divides(number1, number2) {
-  alert(number1 / number2);
+  return number1 / number2;
 }
 
-divides(9, 10);
-divides(4, 60);
+alert(divides(9, 10));
+lert(divides(65, 5));
